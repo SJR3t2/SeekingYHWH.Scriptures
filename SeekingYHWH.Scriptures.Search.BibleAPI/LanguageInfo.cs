bd64de18aa76823ee438e5433ca40ae7edededbe
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 14:20:27 -0600

    added class LanguageInfo

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/LanguageInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SeekingYHWH.Scriptures;
+
+namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal sealed class LanguageInfo : SeekingYHWH.Scriptures.LanguageInfo
+{
+	public CollectionInfo[] Collections { get; set; }
+}
