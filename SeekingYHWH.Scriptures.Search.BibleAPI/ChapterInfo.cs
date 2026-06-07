740713525ac19fe8aa63898942d03586eea1d42e
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 14:21:26 -0600

    added class ChapterInfo

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/ChapterInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal sealed class ChapterInfo
+{
+	public string URL { get; set; }
+}
