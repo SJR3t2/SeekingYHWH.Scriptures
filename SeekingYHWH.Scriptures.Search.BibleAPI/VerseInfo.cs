047189be56d483f104169d5563e05f18cee7d0b6
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 15:09:18 -0600

    added class VerseInfo

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/VerseInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal sealed class VerseInfo
+{
+	public string? Book { get; set; }
+	public string? Chapter { get; set; }
+	public string? Verse { get; set; }
+	public string? Text { get; set; }
+}
