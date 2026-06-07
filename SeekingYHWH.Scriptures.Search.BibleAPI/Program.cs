2e8ba66336f9a0a71f893c87cb8955f7e457d88e
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 12:13:44 -0600

    started program SeekingYHWH.Scriptures.Search.BibleAPI

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal static class  Program
+{
+	private static int Main(string[] args)
+	{
+		Prepare();
+		return Execute();
+	}
+
+	private static void Prepare()
+	{
+	}
+
+	private static int Execute()
+	{
+		return 0;
+	}
+}
