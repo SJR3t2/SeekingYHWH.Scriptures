e56e5e6d55d89ca6b9fc3723dbba16dfb149be7b
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 14:19:41 -0600

    added class CollectionInfo

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/CollectionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SeekingYHWH.Scriptures;
+
+namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal sealed class CollectionInfo : SeekingYHWH.Scriptures.BookInfo
+{
+	public string Id { get; set; }
+	public string[] Books { get; set; }
+}
