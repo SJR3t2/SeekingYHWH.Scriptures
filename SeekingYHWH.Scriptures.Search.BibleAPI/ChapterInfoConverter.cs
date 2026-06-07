f19808a5c1b4ef53b73767c919d73445cbbb44fe
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 14:31:10 -0600

    added class internal static class ChapterInfoConverter

---
+++ SeekingYHWH.Scriptures.Search.BibleAPI/ChapterInfoConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+using Innovoft.Text.JSON;
+
+namespace SeekingYHWH.Scriptures.Search.BibleAPI;
+
+internal static class ChapterInfoConverter
+{
+	private static readonly byte[] urlVersion = Encoding.UTF8.GetBytes("url");
+
+	public static void Parse(ChapterInfo value, UTF8JSONReaderStream stream, ref Utf8JsonReader reader)
+	{
+		while(true)
+		{
+			if (!stream.Read(ref reader))
+			{
+				throw new EndOfStreamException();
+			}
+			if (reader.TokenType == JsonTokenType.EndObject)
+			{
+				return;
+			}
+			if (reader.TokenType != JsonTokenType.PropertyName)
+			{
+				throw new FormatException();
+			}
+			if (reader.ValueTextEquals(urlVersion))
+			{
+				if (!stream.Read(ref reader))
+				{
+					throw new EndOfStreamException();
+				}
+				value.URL = reader.GetString();
+			}
+			else
+			{
+				if (!stream.TrySkip(ref reader))
+				{
+					throw new EndOfStreamException();
+				}
+			}
+		}
+	}
+}
