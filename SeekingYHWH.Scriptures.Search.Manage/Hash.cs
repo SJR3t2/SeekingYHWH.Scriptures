5fb7fc500b3aa67eeaf6cc89feb152162f713b97
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-07 21:59:14 -0600

    added method Hash.Compute

---
+++ SeekingYHWH.Scriptures.Search.Manage/Hash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures;
+
+public static class Hash
+{
+	public static void Compute(string sourcePath, string hashPath)
+	{
+		byte[] hash;
+		using (var hasher = SHA256.Create())
+		using (var reader = new FileStream(sourcePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+		{
+			hash = hasher.ComputeHash(reader);
+		}
+
+		using (var writer = new FileStream(hashPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+		{
+			writer.Write(hash, 0, hash.Length);
+		}
+	}
+}
