7165cefd07e9195a7cb55bbe2bc352342b51d2a7
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-09 00:13:44 -0600

    added method Book.OpenTSV

---
+++ SeekingYHWH.Scriptures.Search.Manage/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures;
+
+public static class Book
+{
+	public static StreamWriter OpenTSV(string path)
+	{
+		var writerStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);
+		var writer = new StreamWriter(writerStream);
+		return writer;
+	}
+}
