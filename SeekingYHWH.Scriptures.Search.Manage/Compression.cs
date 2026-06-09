5bfa666edcc267babb6a83e608456db8cc357335
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-08 23:41:38 -0600

    added method Compression.Compress

---
+++ SeekingYHWH.Scriptures.Search.Manage/Compression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures;
+
+public static class Compression
+{
+	public static void Compress(string tsvPath, string brPath)
+	{
+		using (var writerStream = new FileStream(brPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+		using (var writer = new BrotliStream(writerStream, CompressionLevel.SmallestSize))
+		using (var reader = new FileStream(tsvPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+		{
+			reader.CopyTo(writer);
+		}
+	}
+}
