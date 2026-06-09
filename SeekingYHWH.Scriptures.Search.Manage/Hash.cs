095df64e8ee910031bc5af86833a4099ec370099
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-08 23:47:24 -0600

    added method ComputeBR

--- SeekingYHWH.Scriptures.Search.Manage/Hash.cs
+++ SeekingYHWH.Scriptures.Search.Manage/Hash.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -11,12 +12,26 @@ public static class Hash
 	public static void ComputeTSV(string tsvPath, string hashPath)
 	{
 		byte[] hash;
-		using (var hasher = SHA256.Create())
 		using (var reader = new FileStream(tsvPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+		using (var hasher = SHA256.Create())
 		{
 			hash = hasher.ComputeHash(reader);
 		}
+		using (var writer = new FileStream(hashPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+		{
+			writer.Write(hash, 0, hash.Length);
+		}
+	}
 
+	public static void ComputeBR(string brPath, string hashPath)
+	{
+		byte[] hash;
+		using (var readerFile = new FileStream(brPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+		using (var reader = new BrotliStream(readerFile, CompressionMode.Decompress))
+		using (var hasher = SHA256.Create())
+		{
+			hash = hasher.ComputeHash(reader);
+		}
 		using (var writer = new FileStream(hashPath, FileMode.Create, FileAccess.Write, FileShare.Read))
 		{
 			writer.Write(hash, 0, hash.Length);
