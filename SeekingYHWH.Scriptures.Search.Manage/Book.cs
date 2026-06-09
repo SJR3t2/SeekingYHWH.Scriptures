eec921c729484692c2bff7f6e58335f637e8a685
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-09 00:16:10 -0600

    added method OpenWriterBR

--- SeekingYHWH.Scriptures.Search.Manage/Book.cs
+++ SeekingYHWH.Scriptures.Search.Manage/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace SeekingYHWH.Scriptures;
@@ -10,6 +11,14 @@ public static class Book
 	public static StreamWriter OpenWriterTSV(string path)
 	{
 		var writerStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);
+		var writer = new StreamWriter(writerStream);
+		return writer;
+	}
+
+	public static StreamWriter OpenWriterBR(string path)
+	{
+		var writerFile = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read);
+		var writerStream = new BrotliStream(writerFile, CompressionLevel.SmallestSize);
 		var writer = new StreamWriter(writerStream);
 		return writer;
 	}
