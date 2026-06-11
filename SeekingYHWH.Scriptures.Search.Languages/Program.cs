e86b56d3c92aba04f6a1fad675bc87f062607237
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-11 01:07:36 -0600

    added util SeekingYHWH.Scriptures.Search.Languages

---
+++ SeekingYHWH.Scriptures.Search.Languages/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SeekingYHWH.Scriptures.Search;
+
+internal static class Program
+{
+	private static int Main(string[] args)
+	{
+		return Execute();
+	}
+
+	private static string scripturesPath = @"D:\Projects\SeekingYHWH.Scriptures.Search";
+
+	private static int Execute()
+	{
+		var brPath = LanguageInfosPaths.GetPathBr(scripturesPath);
+		var tsvPath = LanguageInfosPaths.GetPath(scripturesPath);
+		Compression.Decompress(brPath, tsvPath);
+		Console.WriteLine("delete");
+		Console.WriteLine("keep");
+		Console.WriteLine("use");
+		Console.WriteLine();
+		while (true)
+		{
+			Console.Write("Command: ");
+			var input = Console.ReadLine();
+			switch (input)
+			{
+			case "delete":
+				File.Delete(tsvPath);
+				goto Done;
+
+			case "keep":
+				goto Done;
+
+			case "use":
+				Compression.Compress(tsvPath, brPath);
+				File.Delete(tsvPath);
+				var hashPath = LanguageInfosPaths.GetHashPath(scripturesPath);
+				Hash.ComputeBR(brPath, hashPath);
+				goto Done;
+			}
+		}
+	Done:
+		return 0;
+	}
+}
