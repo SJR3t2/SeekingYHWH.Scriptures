24aa773e9ed4f94d8c4c14c169fedecf102ed520
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-19 22:34:36 -0600

    started app SeekingYHWH.Scriptures.ConvertPassages.MAUI

---
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+
+			var convertPage = new ShellContent()
+			{
+				Title = "Convert",
+				Content = new ConvertPage(),
+			};
+			this.Items.Add(convertPage);
+		}
+	}
+}
---
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/ConvertPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI
+{
+	public partial class ConvertPage : ContentPage
+	{
+		int count = 0;
+
+		public ConvertPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnTextChanged(object? sender, EventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnClear(object? sender, EventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnCopy(object? sender, EventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnConvert(object? sender, EventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
