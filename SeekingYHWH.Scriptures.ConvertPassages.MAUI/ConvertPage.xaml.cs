24454312971d14f47a3568bc914789654afacefc
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-24 17:59:27 -0600

    code formatting

--- SeekingYHWH.Scriptures.ConvertPassages.MAUI/ConvertPage.xaml.cs
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/ConvertPage.xaml.cs
@@ -1,35 +1,34 @@
-﻿namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI
+﻿namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI;
+
+public partial class ConvertPage : ContentPage
 {
-	public partial class ConvertPage : ContentPage
+	public ConvertPage()
 	{
-		public ConvertPage()
-		{
-			InitializeComponent();
-		}
+		InitializeComponent();
+	}
 
-		private void OnLoaded(object? sender, EventArgs args)
-		{
-			passagesText.Focus();
-		}
+	private void OnLoaded(object? sender, EventArgs args)
+	{
+		passagesText.Focus();
+	}
 
-		private void OnTextChanged(object? sender, EventArgs args)
-		{
-			throw new NotImplementedException();
-		}
+	private void OnTextChanged(object? sender, EventArgs args)
+	{
+		throw new NotImplementedException();
+	}
 
-		private void OnClear(object? sender, EventArgs args)
-		{
-			throw new NotImplementedException();
-		}
+	private void OnClear(object? sender, EventArgs args)
+	{
+		throw new NotImplementedException();
+	}
 
-		private void OnCopy(object? sender, EventArgs args)
-		{
-			throw new NotImplementedException();
-		}
+	private void OnCopy(object? sender, EventArgs args)
+	{
+		throw new NotImplementedException();
+	}
 
-		private void OnConvert(object? sender, EventArgs args)
-		{
-			throw new NotImplementedException();
-		}
+	private void OnConvert(object? sender, EventArgs args)
+	{
+		throw new NotImplementedException();
 	}
 }
