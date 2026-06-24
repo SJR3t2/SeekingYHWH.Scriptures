f0fca34ff7af4301311e554d4e48d7046927c56c
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-24 17:55:59 -0600

    implamenting OptionsPage

--- SeekingYHWH.Scriptures.ConvertPassages.MAUI/OptionsPage.xaml.cs
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/OptionsPage.xaml.cs
@@ -6,4 +6,94 @@ public partial class OptionsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnLDSBoMS(object sender, EventArgs args)
+	{
+		if (ldsBoMGrid == null)
+		{
+			return;
+		}
+
+		ldsBoMCenterLabel.IsEnabled = true;
+		ldsBoMCenter.IsEnabled = true;
+
+		ldsBoMPre.Text = "(LDS ";
+		ldsBoMCenter.Text = ")(RLDS ";
+		ldsBoMPost.Text = ")";
+	}
+
+	private void OnLDSBoMO(object sender, EventArgs args)
+	{
+		if (ldsBoMGrid == null)
+		{
+			return;
+		}
+
+		ldsBoMCenterLabel.IsEnabled = true;
+		ldsBoMCenter.IsEnabled = true;
+
+		ldsBoMPre.Text = "(RLDS ";
+		ldsBoMCenter.Text = ")(LDS ";
+		ldsBoMPost.Text = ")";
+	}
+
+	private void OnLDSBoMN(object sender, EventArgs args)
+	{
+		if (ldsBoMGrid == null)
+		{
+			return;
+		}
+
+		ldsBoMCenterLabel.IsEnabled = false;
+		ldsBoMCenter.IsEnabled = false;
+
+		ldsBoMPre.Text = string.Empty;
+		ldsBoMCenter.Text = null;
+		ldsBoMPost.Text = string.Empty;
+	}
+
+	private void OnRLDSBoMS(object sender, EventArgs args)
+	{
+		if (rldsBoMGrid == null)
+		{
+			return;
+		}
+
+		rldsBoMCenterLabel.IsEnabled = true;
+		rldsBoMCenter.IsEnabled = true;
+
+		rldsBoMPre.Text = "(RLDS ";
+		rldsBoMCenter.Text = ")(LDS ";
+		rldsBoMPost.Text = ")";
+	}
+
+	private void OnRLDSBoMO(object sender, EventArgs args)
+	{
+		if (rldsBoMGrid == null)
+		{
+			return;
+		}
+
+		rldsBoMCenterLabel.IsEnabled = true;
+		rldsBoMCenter.IsEnabled = true;
+
+		rldsBoMPre.Text = "(LDS ";
+		rldsBoMCenter.Text = ")(RLDS ";
+		rldsBoMPost.Text = ")";
+	}
+
+	private void OnRLDSBoMN(object sender, EventArgs args)
+	{
+		if (rldsBoMGrid == null)
+		{
+			return;
+		}
+
+		rldsBoMCenterLabel.IsEnabled = false;
+		rldsBoMCenter.IsEnabled = false;
+
+		rldsBoMPre.Text = string.Empty;
+		rldsBoMCenter.Text = null;
+		rldsBoMPost.Text = string.Empty;
+	}
 }
