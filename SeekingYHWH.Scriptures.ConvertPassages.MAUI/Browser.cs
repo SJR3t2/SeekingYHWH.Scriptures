01a14bbcf994239b9891afd1582d97fbc52e5137
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-25 11:49:27 -0600

    added class Browser

---
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/Browser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI;
+
+internal static class Browser
+{
+	private static readonly BrowserLaunchOptions externally = new BrowserLaunchOptions() { LaunchMode = BrowserLaunchMode.External };
+	private static readonly BrowserLaunchOptions internally = new BrowserLaunchOptions() { LaunchMode = BrowserLaunchMode.SystemPreferred };
+
+	public static async Task Open(string url)
+	{
+		if (string.IsNullOrWhiteSpace(url))
+		{
+			return;
+		}
+
+		try
+		{
+			await Microsoft.Maui.ApplicationModel.Browser.Default.OpenAsync(url, externally);
+			return;
+		}
+		catch
+		{
+		}
+
+		try
+		{
+			await Microsoft.Maui.ApplicationModel.Browser.Default.OpenAsync(url, internally);
+			return;
+		}
+		catch
+		{
+		}
+	}
+}
