cb56fac557f8d1afd843e14510a7c335bfa3f962
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-26 19:08:54 -0600

    added class GroupedTextDetail

---
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/GroupedTextDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI;
+
+internal sealed class GroupedTextDetail : ObservableCollection<TextDetail>
+{
+	public string? Title { get; set; }
+}
