e526840270f639b0290d851c33c9e1715aeb8e68
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-26 19:06:33 -0600

    added class TextDetail

---
+++ SeekingYHWH.Scriptures.ConvertPassages.MAUI/TextDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeekingYHWH.Scriptures.ConvertPassages.MAUI;
+
+internal sealed class TextDetail
+{
+	public string? Text { get; set; }
+	public string? Detail { get; set; }
+}
